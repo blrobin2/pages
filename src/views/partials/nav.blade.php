9ec495f5edee6b889938eebae47c7e255fdd1567
BT Robinson <brobinson88@gmail.com>
2015-08-06 11:24:17 -0400

    Remove home nav partial, since it's redundant now.

--- src/views/partials/nav.blade.php
+++ src/views/partials/nav.blade.php
@@ -2,9 +2,6 @@
 
 <nav class="nav --main" role="navigation">
 	<ul class="nav__list">
-		<li class="nav__item">
-			<a class="link nav__link" href="{{ url('/') }}">Home</a>
-		</li>
 		@foreach($pages->unhidden()->get() as $page)
 			<li class="nav__item">
 				<a class="link nav__link" href="{{ url($page->link) }}">{{ $page->title }}</a>
